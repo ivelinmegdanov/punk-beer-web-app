83112bd8f2bc1bd56cba914d01b326f345b9b7ad
Ivelin Megdanov <iv4o.megdanov@gmail.com>
2024-03-13 13:35:22 +0200

    Styled the NavBar with bootstrap

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -2,17 +2,36 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 const Navbar = () => (
-  <nav>
-    <Link to="/">Punk Beer App</Link>
-    <div>
-      <ul>
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-        <li>
-          <Link to="/favorites">Favorites</Link>
-        </li>
-      </ul>
+  <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+    <div className="container-fluid">
+      <Link className="navbar-brand" to="/">
+        Punk Beer App
+      </Link>
+      <button
+        className="navbar-toggler"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#navbarNav"
+        aria-controls="navbarNav"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span className="navbar-toggler-icon"></span>
+      </button>
+      <div className="collapse navbar-collapse" id="navbarNav">
+        <ul className="navbar-nav ms-auto">
+          <li className="nav-item">
+            <Link className="nav-link" aria-current="page" to="/">
+              Home
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link className="nav-link" to="/favorites">
+              Favorites
+            </Link>
+          </li>
+        </ul>
+      </div>
     </div>
   </nav>
 );
