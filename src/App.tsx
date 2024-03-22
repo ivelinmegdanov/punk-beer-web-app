d7da2dfc08ff27894131a16eb9d417551227ae77
Ivelin Megdanov <iv4o.megdanov@gmail.com>
2024-03-22 17:34:37 +0200

    Removed Web3 test page

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,9 @@ import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import Favorites from "./pages/Favorites";
-import Web3 from "./pages/Web3";
 import NavBar from "./components/NavBar";
 import AddBeer from "./pages/AddBeer";
+import BeerInformation from "./pages/BeerInformation";
 
 function App() {
   return (
@@ -13,9 +13,8 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/favorites" element={<Favorites />} />
-        <Route path="/web3" element={<Web3 />} />
         <Route path="/add-beer" element={<AddBeer />} />
-        {/* Additional routes if needed */}
+        <Route path="/beer/:id" element={<BeerInformation />} />
       </Routes>
     </Router>
   );
